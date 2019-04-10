93ef54a5d9c32b1b5de9e1a100d02363fd391535
Martin Billinger <flkazemakase@gmail.com>
2019-04-10 23:49:04 +0200

    some useful sequence operations

--- utils.scm
+++ utils.scm
@@ -79,6 +79,27 @@
             (append (cdr list1)
                     list2))))
 
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (map1 p sequence)
+  (accumulate (lambda (x y) (cons (p x) y)) nil sequence))
+
+(define (append seq1 seq2)
+  (accumulate cons seq2 seq1))
+
+(define (length sequence)
+  (accumulate (lambda (x y) (+ y 1)) 0 sequence))
+
+;(define (map op . seqs)
+;  (if null? (car seqs)
+;      nil
+;      (cons (eval (cons op (map1 car seqs)))
+;            (map (cons op (map1 cdr seqs)))))
+
 ;; ==========================================
 ;;   useless stuff
 ;; ==========================================
