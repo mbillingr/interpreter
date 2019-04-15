2af301927dfc6b757319b1f93c405b30357defe0
Martin Billinger <flkazemakase@gmail.com>
2019-04-15 21:15:01 +0200

    add procedures: put get, for global table access

--- utils.scm
+++ utils.scm
@@ -125,6 +125,70 @@
              (apply println (cdr args)))))
 
 ;; ==========================================
+;;   put and get into a global table
+;;   (need those in exercise 2.73 and maybe later)
+;;   (this is a very crude implementation using unordered lists)
+;; ==========================================
+
+(define (impl-put-get)
+  (define dispatch-table '())
+
+  (define (make-record key value) (cons key value))
+  (define (key record) (car record))
+  (define (value record) (cdr record))
+
+  (define (lookup given-key set-of-records)
+    (cond ((null? set-of-records) false)
+          ((equal? given-key (key (car set-of-records)))
+           (car set-of-records))
+          (else (lookup given-key (cdr set-of-records)))))
+
+  (define (adjoin-set x set)
+    (if (element-of-set? x set)
+        set
+        (cons x set)))
+
+  (define (insert given-key value set-of-records)
+    (define (iter remaining-set result)
+      (cond ((null? remaining-set)
+             (cons (make-record given-key value)
+                   result))
+            ((equal? given-key (key (car remaining-set)))
+             (append (cons (make-record given-key value)
+                         result)
+                   (cdr remaining-set)))
+            (else (iter (cdr remaining-set)
+                        (cons (car remaining-set)
+                              result)))))
+    (iter set-of-records '()))
+
+  (define (make-key op type)
+    (cons op type))
+
+  (define (put op type item)
+    (set! dispatch-table
+          (insert (make-key op type)
+                  item
+                  dispatch-table)))
+
+  (define (get op type)
+    (let ((record (lookup (make-key op type)
+                          dispatch-table)))
+      (if record
+          (value record)
+          #f)))
+
+  ; export public functions
+  (list put get))
+
+(define put '())
+(define get '())
+(let ((put_get (impl-put-get)))
+  (set! put (car put_get))
+  (set! get (cadr put_get)))
+
+
+;; ==========================================
 ;;   useless stuff
 ;; ==========================================
 
