56ec497144d40869a5c8b3ac9706017e61d242db
Martin Billinger <flkazemakase@gmail.com>
2019-04-30 19:30:59 +0200

    exercise 2.96

--- sicp/generic/math.scm
+++ sicp/generic/math.scm
@@ -1,6 +1,6 @@
 
 (define-library (sicp generic math)
-  (export =zero? add div equ? greatest-common-divisor mul neg sub)
+  (export =zero? add div equ? greatest-common-divisor mul neg sub pow)
 
   (import (sicp generic))
 
@@ -10,6 +10,7 @@
     (define (mul x y) (apply-generic 'mul x y))
     (define (div x y) (apply-generic 'div x y))
     (define (neg x) (apply-generic 'neg x))
+    (define (pow b e) (apply-generic 'pow b e))
 
     (define (equ? a b) (apply-generic 'equ? a b))
     (define (=zero? x) (apply-generic '=zero? x))
--- sicp/generic/scheme-number.scm
+++ sicp/generic/scheme-number.scm
@@ -13,6 +13,7 @@
     (put 'mul '(scheme-number scheme-number) (lambda (x y) (tag (* x y))))
     (put 'div '(scheme-number scheme-number) (lambda (x y) (tag (/ x y))))
     (put 'neg '(scheme-number) (lambda (x) (tag (- x))))
+    (put 'pow '(scheme-number scheme-number) (lambda (x y) (tag (exp x y))))
     (put 'equ? '(scheme-number scheme-number) =)
     (put '=zero? '(scheme-number) (lambda (x) (= x 0)))
     (put 'greatest-common-divisor '(scheme-number scheme-number)
@@ -21,4 +22,15 @@
     (put 'make 'scheme-number (lambda (x) (tag x)))
 
     (define (make-scheme-number n)
-      ((get 'make 'scheme-number) n))))
+      ((get 'make 'scheme-number) n))
+
+    (define (exp base exponent)
+      (define (exp-iter a b n)
+        (cond ((= n 0) a)
+              ((even? n) (exp-iter a
+                                   (* b b)
+                                   (/ n 2)))
+              (else (exp-iter (* a b)
+                              b
+                              (- n 1)))))
+      (exp-iter 1 base exponent))))
