55748087ee3818b3d1d74f98a03cd5c24b1f6930
Martin <flkazemakase@gmail.com>
2019-05-14 09:03:34 +0200

    add test for apply

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,6 +1,8 @@
 mod r7rs_6_2_equivalence_predicates;
+mod r7rs_procedure_apply;
 
 use crate::environment::default_env;
+use crate::envref::EnvRef;
 use crate::errors::Result;
 use crate::eval;
 use crate::parser::parse;
@@ -9,8 +11,11 @@ use crate::Expression;
 use crate::Lexer;
 
 fn run(src: &str) -> Result<Expression> {
+    run_in_env(src, &default_env())
+}
+
+fn run_in_env(src: &str, env: &EnvRef) -> Result<Expression> {
     let mut lexer = Lexer::new();
-    let env = default_env();
     let tokens = lexer
         .tokenize(src)?
         .take()
@@ -19,7 +24,7 @@ fn run(src: &str) -> Result<Expression> {
 
     parse(tokens, src.to_string())?
         .into_iter()
-        .map(|expr| expand(&expr, &env))
+        .map(|expr| expand(&expr, env))
         .map(|expr| eval(&expr?, env.clone()))
         .last()
         .unwrap()
---
+++ src/tests/r7rs_procedure_apply.rs
@@ -0,0 +1,30 @@
+use super::run_in_env;
+use crate::environment::{default_env, Environment};
+use crate::envref::EnvRef;
+use crate::Expression;
+
+#[test]
+fn apply() {
+    let env = default_env();
+    env.borrow_mut().insert_native("sqrt", |args| {
+        args.car()
+            .and_then(|x| x.try_as_float())
+            .map(f64::sqrt)
+            .map(Expression::from)
+    });
+
+    assert_eq!(
+        Expression::Integer(7),
+        run_in_env("(apply +  (list 3 4))", &env).unwrap()
+    );
+
+    run_in_env(
+        "(define compose (lambda (f g) (lambda args (f (apply g args)))))",
+        &env,
+    )
+    .unwrap();
+    assert_eq!(
+        Expression::Float(30.0),
+        run_in_env("((compose sqrt *) 12 75)", &env).unwrap()
+    );
+}
